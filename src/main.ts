dcb4452b2c825e4c3c167272315e0afce40d7bef
Stickle <64675913+The-True-Stickle@users.noreply.github.com>
2025-09-25 11:59:20 -0700

    Added the class GameManager which holds game states such as the current day. Also added a function named PrintOutDay to print out the current day.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 console.log("Hello, World! It's lemon time!");
+PrintOutDay(5, 89, 4);
 
 class LemonadeStand {
     currentCash: number;
+}
+
+class GameManager {
+    currentDay: number;
+    currentWeather: number;
+    currentLemonadePrice: number;
+
+}
+
+function PrintOutDay (currentDay: number, currentWeather: number, currentLemonadePrice: number) {
+    console.log("This is day " + currentDay + "\n");
+    console.log("The weather is " + currentWeather + " degrees \n");
+    console.log("Lemonade costs " + currentLemonadePrice + " dollars\n");
+    
+
 }
