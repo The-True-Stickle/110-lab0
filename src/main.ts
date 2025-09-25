dc5ef7105e101d2309459986db742cef26e96442
Stickle <64675913+The-True-Stickle@users.noreply.github.com>
2025-09-25 12:16:31 -0700

    Added a random integer generator which will be used to determine the current weather and lemon price.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-console.log("Hello, World! It's lemon time!");
-PrintOutDay(5, 89, 4);
+
 
 class LemonadeStand {
     currentCash: number;
@@ -19,3 +18,17 @@ function PrintOutDay (currentDay: number, currentWeather: number, currentLemonad
     
 
 }
+
+//Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(minimumNum: number, maximumNum: number) {
+    const minCeiling = Math.ceil(minimumNum);
+    const maxFloor = Math.floor(maximumNum);
+    return Math.floor(Math.random() * (maxFloor - minCeiling + 1) + minCeiling);
+}
+
+console.log("Hello, World! It's lemon time!");
+const gameManager = new GameManager();
+gameManager.currentDay = 1;
+gameManager.currentWeather = getRandomInt(15, 115);
+gameManager.currentLemonadePrice = getRandomInt(15, 115);
+PrintOutDay(gameManager.currentDay, gameManager.currentWeather, gameManager.currentLemonadePrice);
